7e3fe3311437fc54e89fb3e694440f97a225482c
ReggieVeggieIQ <crycall6@gmail.com>
2023-10-02 11:49:59 +0200

    css javascript and pages

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from website.views import HomepageView
+from website.views import HomepageView, LoginView, RegisterView
 
 urlpatterns = [
     path("", HomepageView.as_view(), name='index'),
+    path("login/", LoginView.as_view(), name='login'),
+    path("register/", RegisterView.as_view(), name='register'),
 ]
