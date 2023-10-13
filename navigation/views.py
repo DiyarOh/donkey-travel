0eb5babe8d64d41eaa788b993dde3b0ea4d203ad
CaptainSpazmo <mikeyheinrici@gmail.com>
2023-10-13 14:43:28 +0200

    routes post
    routes are now able to be drawn and posted

--- navigation/views.py
+++ navigation/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 from django.views.generic import View
@@ -25,5 +26,18 @@ class RouteView(View):
         donkey_travel = LandMark.objects.filter(name='Donkey Travel').get()
         return render(request, template_name, {"markers": markers, "donkeytravel": donkey_travel})
 
-    def post(self, request, log):
-        pass
+    def post(self, request):
+        try:
+            data = request.body.decode('utf-8')
+            # You can print the received data
+            print(data)
+            
+            # Process and save the LineString data from the request
+            # Your processing logic here
+
+            # Return a JSON response to indicate success
+            response_data = {'message': 'LineString data saved successfully'}
+            return JsonResponse(response_data)
+        except Exception as e:
+            # Handle any exceptions and return an error response
+            response_data = {'error': str(e)}
