18262faaf7960c93a49e5f74c225395dd34bee2e
ReggieVeggieIQ <crycall6@gmail.com>
2023-10-25 08:55:29 +0200

    BUILD OF WEBSITE

--- website/views.py
+++ website/views.py
@@ -13,3 +13,71 @@ class LoginView(TemplateView):
 
 class RegisterView(TemplateView):
     template_name = "register.html"
+
+
+class BookingsView(TemplateView):
+    template_name = "bookings.html"
+
+
+class MapView(TemplateView):
+    template_name = "map.html"
+
+
+class AccountView(TemplateView):
+    template_name = "account.html"
+
+
+class AccountLoginView(TemplateView):
+    template_name = "accountlogin.html"
+
+
+class BookingCreateView(TemplateView):
+    template_name = "bookingcreate.html"
+
+
+class Bookings2View(TemplateView):
+    template_name = "bookings2.html"
+
+
+class ChangeRouteView(TemplateView):
+    template_name = "changeroute.html"
+
+
+class GuestDashboardView(TemplateView):
+    template_name = "guestdashboard.html"
+
+
+class GuestBookingsView(TemplateView):
+    template_name = "guestbookings.html"
+
+
+class Map2View(TemplateView):
+    template_name = "map2.html"
+
+
+class RoutesView(TemplateView):
+    template_name = "routes.html"
+
+
+class StaffAccommodationsView(TemplateView):
+    template_name = "staffaccommodations.html"
+
+
+class StaffBookingsView(TemplateView):
+    template_name = "staffbookings.html"
+
+
+class StaffDashboardView(TemplateView):
+    template_name = "staffdashboard.html"
+
+
+class CreateBookingView(TemplateView):
+    template_name = "createbooking.html"
+
+
+class AccommodationsView(TemplateView):
+    template_name = "accommodations.html"
+    
+
+class DeleteAccommodationsView(TemplateView):
+    template_name = "deleteaccommodations.html"
